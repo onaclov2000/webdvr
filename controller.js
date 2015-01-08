b30d608bb6128579ab320e6e9e98165c75a7c907
Tyson Bailey <onaclov2000+github@gmail.com>
2015-01-07 22:37:33 -0700

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,13 +1,33 @@
 angular.module('myApp', ['firebase'])
 .controller("MyCtrl", ["$scope", "$firebase",
   function($scope, $firebase) {
-    var ref = new Firebase('https://onaclovtech-home.firebaseio.com/dvr/');
+    var ref = new Firebase('');
     var sync = $firebase(ref);
 
     // if ref points to a data collection
     $scope.list = sync.$asArray();
-
+    $scope.addSet = true;
+    $scope.addSchedule = false;
     // if ref points to a single record
     $scope.rec = sync.$asObject();
+    $scope.schedule = function (){
+       var oldDateObj = new Date();
+       var newDateObj = new Date(oldDateObj.getTime() + 30*60000);
+       sync.$update("status", { channel : $scope.selectedStation.channel, 
+                                program : $scope.selectedStation.program, 
+                                day     : newDateObj.getDate(), 
+                                month   : newDateObj.getMonth(), 
+                                year    : newDateObj.getFullYear(), 
+                                hh      : newDateObj.getHours(), 
+                                mm : newDateObj.getMinutes() });
+      $scope.addSet = false;
+      $scope.addSchedule = true;
+    };
+
+$scope.scheduled = function (){
+       sync.$update("status", { commanded : 'new'});
+       $scope.addSet = true;
+       $scope.addSchedule = false;
+    };
   }
 ]);
