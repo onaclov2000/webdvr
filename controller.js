e32d4710544709080b889e1ebf904695e1ace456
Tyson Bailey <onaclov2000+github@gmail.com>
2015-01-10 04:06:51 -0700

    Update controller.js

--- controller.js
+++ controller.js
@@ -8,26 +8,44 @@ angular.module('myApp', ['firebase'])
     $scope.list = sync.$asArray();
     $scope.addSet = true;
     $scope.addSchedule = false;
+    $scope.times = [1,2,3,4,5,6,7,8,9,10,11,12];
+    $scope.am_pms = {0 : 'am',
+                    12 : 'pm'};
+    $scope.half_hours = {0 : '00',
+                        30 : '30'};
     // if ref points to a single record
     $scope.rec = sync.$asObject();
     $scope.schedule = function (){
-       var oldDateObj = new Date();
-       var newDateObj = new Date(oldDateObj.getTime() + 30*60000);
+       var newDateObj = new Date();
+       if ($scope.selectedAmPm == 'pm'){
+       $scope.selectedTime += 12;
+       }
+
        sync.$update("status", { channel : $scope.selectedStation.channel, 
                                 program : $scope.selectedStation.program, 
                                 day     : newDateObj.getDate(), 
                                 month   : newDateObj.getMonth(), 
                                 year    : newDateObj.getFullYear(), 
-                                hh      : newDateObj.getHours(), 
-                                mm : newDateObj.getMinutes() });
+                                hh      : $scope.selectedTime, 
+                                mm : $scope.selectedHalfHour });
+
       $scope.addSet = false;
       $scope.addSchedule = true;
     };
 
 $scope.scheduled = function (){
-       sync.$update("status", { commanded : 'new'});
-       $scope.addSet = true;
-       $scope.addSchedule = false;
+       if ($scope.selectedStation && $scope.selectedTime && $scope.selectedHalfHour && $scope.selectedAmPm){
+          sync.$update("status", { commanded : 'new'});
+          $scope.addSet = true;
+          $scope.addSchedule = false;
+          $scope.selectedStation = "";
+          $scope.selectedTime = "";
+          $scope.selectedHalfHour = "";
+          $scope.selectedAmPm = "";
+      }
+     // $scope. = "";
+     // $scope. = "";
+     // $scope. = ""; 
     };
   }
 ]);
