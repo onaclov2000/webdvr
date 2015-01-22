3990e47b95ee0bd13bcb9a533fc92cfb248f5f16
Tyson Bailey <onaclov2000+github@gmail.com>
2015-01-22 00:27:08 -0700

    Update tvguide.js

--- tvguide.js
+++ tvguide.js
@@ -11,7 +11,7 @@ module.exports = {
                 var options = {
                     host: 'mobilelistings.tvguide.com',
                     port: 80,
-                    path: '/Listingsweb/ws/rest/schedules/20385.268435456/start/' + start + '/duration/' + length + '?ChannelFields=Name%2CFullN$
+                    path: '/Listingsweb/ws/rest/schedules/20385.268435456/start/' + start + '/duration/' + length + '?ChannelFields=Name%2CFullName%2CNumber%2CSourceId&ScheduleFields=ProgramId%2CEndTime%2CStartTime%2CTitle%2CAiringAttrib%2CCatId&formattype=json&disableChannels=music%2Cppv%2C24hr&inclchTypeMask=16'
                 };
 
                 http.get(options, function(res) {
