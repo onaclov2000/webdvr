4a81ea5012f280fbb3026e9802bcd1b13f9fccaa
Tyson Bailey <onaclov2000+github@gmail.com>
2015-01-22 01:16:55 -0700

    Update dvr.js

--- dvr.js
+++ dvr.js
@@ -7,27 +7,28 @@ var os = require('os')
 
 
 module.exports = {
-   initialize : function (){
-   var interfaces = os.networkInterfaces();
-var addresses = [];
-for (k in interfaces) {
-    for (k2 in interfaces[k]) {
-        var address = interfaces[k][k2];
-        if (address.family == 'IPv4' && !address.internal) {
-            addresses.push(address.address)
+    initialize: function() {
+        var interfaces = os.networkInterfaces();
+        var addresses = [];
+        for (k in interfaces) {
+            for (k2 in interfaces[k]) {
+                var address = interfaces[k][k2];
+                if (address.family == 'IPv4' && !address.internal) {
+                    addresses.push(address.address)
+                }
+            }
         }
+        // first time let's make sure we have a legit listing
+        if (!myRootRef.tvguide) {
+            tvguide.get(Math.floor((new Date).getTime() / 1000), 1440, function(result) {
+                console.log(result[0].Channel);
+                myRootRef.update({
+                    "tvguide": result
+                });
+            });
+
+        }
+
+
     }
-}
-// first time let's make sure we have a legit listing
-if (!myRootRef.tvguide)
-{
-tvguide.get(Math.floor((new Date).getTime() / 1000), 1440, function(result) {
-       console.log(result[0].Channel);
-       myRootRef.update({"tvguide" : result});
-  });
-
-}
-
-
-   }
 }
