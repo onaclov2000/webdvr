1f18e3c720e54d202ce925f48a7dadb3e6c1d9a7
Tyson Bailey <onaclov2000+github@gmail.com>
2015-01-22 00:37:51 -0700

    Update tvguide.js

--- tvguide.js
+++ tvguide.js
@@ -3,25 +3,29 @@ var http = require("http");
 
 
 module.exports = {
-        get: function(date, length) {
-            //var length = 1440; // minutes in a day
-            //var start = Math.floor((new Date).getTime() / 1000);;
-            var start =
-                console.log(start);
-            var options = {
-                host: 'mobilelistings.tvguide.com',
-                port: 80,
-                path: '/Listingsweb/ws/rest/schedules/20385.268435456/start/' + start + '/duration/' + length + '?ChannelFields=Name%2CFullName%2CNumber%2CSourceId&ScheduleFields=ProgramId%2CEndTime%2CStartTime%2CTitle%2CAiringAttrib%2CCatId&formattype=json&disableChannels=music%2Cppv%2C24hr&inclchTypeMask=16'
-            };
+    get: function(date, length) {
+        //var length = 1440; // minutes in a day
+        //var start = Math.floor((new Date).getTime() / 1000);;
+        var start = date;
 
-            http.get(options, function(res) {
-                    console.log("Got response: " + res.statusCode);
-                    //console.log(res);
-                    var str = "";
-                    res.on('data', function(chunk) {
-                        //console.log('BODY: ' + chunk);
-                        str += chunk;
-                    });
-                }
-            }
-        }
+        var options = {
+            host: 'mobilelistings.tvguide.com',
+            port: 80,
+            path: '/Listingsweb/ws/rest/schedules/20385.268435456/start/' + start + '/duration/' + length + '?ChannelFields=Name%2CFullName%2CNumber%2CSourceId&ScheduleFields=ProgramId%2CEndTime%2CStartTime%2CTitle%2CAiringAttrib%2CCatId&formattype=json&disableChannels=music%2Cppv%2C24hr&inclchTypeMask=16'
+        };
+
+        http.get(options, function(res) {
+            console.log("Got response: " + res.statusCode);
+            //console.log(res);
+            var str = "";
+            res.on('data', function(chunk) {
+                //console.log('BODY: ' + chunk);
+                str += chunk;
+            });
+            res.on('end', function() {
+                callback(str);
+            });
+        });
+    }
+
+}
