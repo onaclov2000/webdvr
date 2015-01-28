da6374de5a5d7a2a0e321d24deb489972d5378a6
Tyson Bailey <onaclov2000+github@gmail.com>
2015-01-28 00:22:36 -0700

    Update record.sh

--- record.sh
+++ record.sh
@@ -3,6 +3,3 @@
 hdhomerun_config 103DA852 set /tuner0/channel auto:$2
 hdhomerun_config 103DA852 set /tuner0/program $3
 timeout $4 hdhomerun_config 103DA852 save /tuner0 /media/dvr/dvr/$1.ts
-sleep $4
-sleep 5000
-avconv -i /media/dvr/dvr/$1.ts -vcodec libx264 -ac 2 -strict experimental /media/dvr/dvr/$1.m4v
