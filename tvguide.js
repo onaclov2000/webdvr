385b97a808f5992c194372387f0a654b6e4ec32f
Tyson <onaclov2000+github@gmail.com>
2015-02-25 04:37:31 -0700

    Temp place holder, having funky for loop issues.

--- tvguide.js
+++ tvguide.js
@@ -15,7 +15,6 @@ module.exports = {
         http.get(options, function(res) {
             var str = "";
             res.on('data', function(chunk) {
-                //console.log('BODY: ' + chunk);
                 str += chunk;
             });
             res.on('end', function() {
@@ -24,6 +23,42 @@ module.exports = {
             });
         });
     },
+    shows: function(entire_listing, show) {
+        var res = {};
+        console.log(entire_listing);
+        for (keys in entire_listing){
+           // grab the index.
+           console.log(entire_listing[keys]["ProgramSchedules"]);
+           for (items in entire_listing[keys]["ProgramSchedules"]){  
+               console.log(items);      
+               console.log(keys);     
+               cosole.log(entire_listing[keys]);
+                /*
+                if (entire_listing[keys]["ProgramSchedules"][items]["Title"].indexOf(show) > -1){
+		        startTime = entire_listing[keys]["ProgramSchedules"][items]["StartTime"];
+		        endTime = entire_listing[keys]["ProgramSchedules"][items]["EndTime"];
+		        var scheduledDate = new Date((startTime - 120) * 1000);
+		        // always record 2 minutes before and 2 minutes after approx
+		        var duration = (endTime - startTime) + 240;
+		        res.push({
+		            // Firebase Workaround for not allowed to have .'s in paths
+		            program: entire_listing[keys]["Channel"]["Number"].replace('.', '-'),
+		            day: scheduledDate.getDate(),
+		            month: scheduledDate.getMonth(),
+		            year: scheduledDate.getFullYear(),
+		            hh: scheduledDate.getHours(),
+		            mm: scheduledDate.getMinutes(),
+		            length: duration,
+		            id: entire_listing[keys]["ProgramSchedules"][items]["ProgramId"],
+		            title: entire_listing[keys]["ProgramSchedules"][items]["Title"]
+		        });
+                }*/
+           }
+        } 
+//        console.log(res);
+        return res;
+    },
+
     //        http://mobilelistings.tvguide.com/Listingsweb/ws/rest/airings/20385.268435456/start/1422793800/duration/20160/search?search=Big%20Bang%20Theory&formattype=json
     search: function(program_name, callback) {
         var options = {
@@ -35,7 +70,6 @@ module.exports = {
         http.get(options, function(res) {
             var str = "";
             res.on('data', function(chunk) {
-                //console.log('BODY: ' + chunk);
                 str += chunk;
             });
             res.on('end', function() {
@@ -46,7 +80,7 @@ module.exports = {
     },
     get_name: function(id, callback) {
         //          http://mapi.tvguide.com/listings/details?program=
-        //         console.log(id);
+
         var options = {
             host: 'mapi.tvguide.com',
             port: 80,
@@ -56,13 +90,11 @@ module.exports = {
         http.get(options, function(res) {
             var str = "";
             res.on('data', function(chunk) {
-                //console.log('BODY: ' + chunk);
                 str += chunk;
             });
 
             res.on('end', function() {
                 var result = JSON.parse(str);
-                //          console.log("get_name: " + result);
                 if (result["program"] != null) {
                     if (result["program"]["season"] != null && result["program"]["episode"] != null && result["program"]["episode_title"] != null) {
                         callback(sanitize("S" + result["program"]["season"] + ".E" + result["program"]["episode"] + "." + result["program"]["title"] + "." + result["program"]["episode_title"]).replace(/ /g, "_"));
