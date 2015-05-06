620728c5c417421933afb959bccf845b99634b00
Tyson Bailey <onaclov2000+github@gmail.com>
2015-05-06 16:32:04 -0600

    Update dvr.js

--- dvr.js
+++ dvr.js
@@ -100,18 +100,4 @@ function onComplete() {
     process.exit();
 }
 
-// Scheduler file.
-function conflict(time1, duration1, time2, duration2){
-   if ((time1 < time2) && (time2 < time1 + duration1)){
-      return true;
-   }
-   if ((time2 < time1) && (time1 < time2 + duration2)){
-      // so we've found a conflict
-      return true;
-   }
-   if (time1 == time2){
-      return true;
-   }
 
-   return false;
-}
