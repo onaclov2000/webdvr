1e900f51c71726fde83cb2598ee4545ff3b1187a
Tyson Bailey <onaclov2000+github@gmail.com>
2015-05-06 16:15:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -11,31 +11,6 @@ var dvr = require('./dvr')
 dvr.start(); // is a "start" even necessary?
 
 
-
-
-// add loop to schedule when starting up.
-
-// When we see that a commanded record has taken place we should do something about it. 
-myRootRef.on('child_changed', function(childSnapshot, prevChildName) {
-    // code to handle child data changes.
-    var data = childSnapshot.val();
-    var localref = childSnapshot.ref();
-    if (data["commanded"] == "new") {
-        localref.update({
-            "commanded": "waiting"
-        });
-        var date = new Date(data["year"], data["month"], data["day"], data["hh"], data["mm"], 0);
-
-        console.log("New Schedule Added " + data["title"] + " @");
-        console.log(date);
-        // queue in this case means we need to make sure we keep track of all our recordings
-        // I'm open to new names but this will be sufficient for now
-        dvr.queue(date, data["program"], data["length"], data["title"], data["id"]);
-        // Scheduling only occurs and is controlled by the "job scheduler"
-        
-    }
-});
-
 // Do something when app is closing
 process.on('exit', dvr.cleanup());
 
