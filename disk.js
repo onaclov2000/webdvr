b28c6b1d513132a44f5e06b23688d21c78288755
Tyson Bailey <onaclov2000+github@gmail.com>
2015-05-06 16:17:48 -0600

    Update disk.js

--- disk.js
+++ disk.js
@@ -10,31 +10,8 @@ module.exports = {
                     for(var i = 0; i<data.length; i++)
                     {
                         if (data[i].drive.indexOf('/dev/sda1') > -1){
-/*
-                        // Get drive mount point 
-                        console.log(data[i].mountpoint);
-
-                        // Get drive total space 
-                        console.log(data[i].total);
-
-                        // Get drive used space 
-                        console.log(data[i].used);
-*/
-                        // Get drive available space
-                        //console.log(data[i].available);
-                        console.log(Math.floor(data[i].available.split(' ')[0] / 3.7));
-                        result(Math.floor(data[i].available.split(' ')[0] / 3.7));
-/*
-
-                        // Get drive name
-                        console.log(data[i].drive);
-
-                        // Get drive used percentage
-                        console.log(data[i].usedPer);
-
-                        // Get drive free percentage
-                        console.log(data[i].freePer);
-*/
+   //                        console.log(Math.floor(data[i].available.split(' ')[0] / 3.7));
+                           result(Math.floor(data[i].available.split(' ')[0] / 3.7));
                         }
                     }
 
