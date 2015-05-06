0611c7656ed19f49e68c2b0e1e3513978a637b7f
Tyson Bailey <onaclov2000+github@gmail.com>
2015-05-06 16:13:29 -0600

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -13,3 +13,33 @@ var j = schedule.scheduleJob(rule, function() {
         });
 
 }
+
+// This should be moved to a 'scheduler', in theory we run through the jobs once, then when the snapshot changes we can re-assess whether we should schedule another.
+myRootRef.child("jobs").once('child_changed', function(childSnapshot) {
+// This needs to be re-worked
+/*
+if (childSnapshot.val() != null){
+for (var key in childSnapshot.val()){
+var x = childSnapshot.val()[key];
+if (myRootRef != null){
+var Today = new Date().getTime();
+var schedule = new Date(x["date"]).getTime();
+if (schedule + (x["length"] * 1000) > Today){
+dvr.schedule(new Date(x["date"]), myRootRef, x["channel"], x["length"], x["title"], x["id"]);
+}
+else{
+myRootRef.child("jobs").child(key).remove();
+console.log("old Show");
+}
+}
+else{
+console.log("Ref is null");
+}
+}
+dvr.cleanup_jobs();
+}
+else{
+console.log("No Outstanding Jobs Left To Schedule");
+}
+*/
+});
