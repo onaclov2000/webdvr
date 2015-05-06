eefe1a0db681e21635a76e294fb9ea4903826759
Tyson Bailey <onaclov2000+github@gmail.com>
2015-05-06 16:30:04 -0600

    Update disk.js

--- disk.js
+++ disk.js
@@ -9,6 +9,13 @@ module.exports = {
                 function (err, data) {
                     for(var i = 0; i<data.length; i++)
                     {
+                        
+                            // perhaps update the reference to firebase whenever the value changes? but only return the value to the scheduler, 
+                   //disk.time(function(res){
+                   //   myRootRef.update({
+                   //      "time_remaining": res
+                   //   });
+                  // });         
                         if (data[i].drive.indexOf('/dev/sda1') > -1){
    //                        console.log(Math.floor(data[i].available.split(' ')[0] / 3.7));
                            result(Math.floor(data[i].available.split(' ')[0] / 3.7));
