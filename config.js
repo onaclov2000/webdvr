f582271ced9c58e57933fe9e959bdeda9698ded6
Tyson Bailey <onaclov2000+github@gmail.com>
2015-05-06 21:46:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-   FB_URL : '' // full path to your Firebase URL, other config items will also fall into this in the future.
+   FB_URL : '', // full path to your Firebase URL, other config items will also fall into this in the future.
+   UPDATE_FREQUENCY : {hour : 11, minute : 59, duration : 1500} //Default is 12 hour updates, with 25 hr duration
 }
