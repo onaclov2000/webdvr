4e7a1f4951646586d4bbc3a0f0d8c14e3392eaec
Tyson Bailey <onaclov2000+github@gmail.com>
2015-06-04 09:43:54 -0600

    Fix queue to use new instances whenever it's requried in.

--- queue.js
+++ queue.js
@@ -1,62 +1,57 @@
-var filter = require('./filter');
-var local_queue = [];
-
-// Expectations. 
+var sort = require('./sort');
+// Expectations.
 // End time > start time
-
-
-var entire = function(){
-   return local_queue;
+var entire = function(self) {
+    return self.local_queue;
+}
+var add = function(self, obj, res) {
+            var temp = new Date().getTime();
+            if (obj.endTime > temp) {
+                // push to local queue
+                self.local_queue.push(obj);
+                // expire the queue element (aka remove at predetermined time)
+                
+				setTimeout(function(val) {
+                        var obj;
+                        for (i = 0; i <= self.local_queue.length - 1; i++) {
+                            if (self.local_queue[i].endTime == val[0]) {
+                                obj = self.local_queue[i];
+                                self.local_queue.splice(i, 1);
+                                val[1](obj);
+                            }
+                        }
+                    }, (obj.endTime - obj.startTime) + 10, [obj.endTime, res])
+					
+                    // Sort Queue by end time by default.
+                self.local_queue.sort(sort.endTime);
+            } else {
+                res(obj);
+            }
+            return self.local_queue;
+        };
+var remove = function(self) {
+    // remove last item
+    self.local_queue.splice(self.local_queue.length - 1, 1);
+    return self.local_queue;
+}
+var element = function() {
+    // grab "head" of queue
+    return self.local_queue[self.local_queue.length - 1];
 }
 
-var add = function (obj, res) {
-    var temp = new Date().getTime();
-    if (obj.endTime > temp)
-    {
-       // push to local queue
-       local_queue.push(obj);
+var queue = function() {
+    var self = this;
+    self.local_queue = [];
+    var methods = {
+        entire: function(){return entire(self)},
+        add: function(obj, res){return add(self, obj, res)},
+        remove: function(){return remove(self)},
+        element: function(){return element(self)}
+    };
 
-       // expire the queue element (aka remove at predetermined time)
-       setTimeout(function(val){
-          var obj;
-          console.log('Timeout value' + val[0]);
-          for (i=0; i <= local_queue.length - 1; i++)
-          {
-             if (local_queue[i].endTime == val[0]){
-                obj = local_queue[i];
-                local_queue.splice(i,1);
-                console.log("this it?");
-                val[1](obj);           
-             }
-          }
-       },
-       (obj.endTime - obj.startTime) + 10,
-       [obj.endTime, res])
+    return methods;
 
-    // Sort Queue by end time by default.
-    local_queue.sort(filter.endTime);
-    }
-    else{
-       res(obj);
-    }
-    return local_queue;
-};
-
-var remove = function(){
-   // remove last item
-   local_queue.splice(local_queue.length-1, 1);
-   return local_queue;
 }
-
-var element = function(){
-   // grab "head" of queue
-   return local_queue[local_queue.length-1];
-}
-
-
-module.exports = {
-   entire : entire,
-   add : add,
-   remove : remove,
-   element : element
+module.exports = function() {
+    return new queue();
 }
